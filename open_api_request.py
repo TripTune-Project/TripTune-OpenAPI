3b72742e089bfd9d5c4e0d64d1c7490bb3f35a5e
Heesoo <soyamilk0705@naver.com>
2024-08-11 22:43:14 +0900

    [feat] 국내 도시 데이터 호출 #1

--- open_api_request.py
+++ open_api_request.py
@@ -1,34 +1,71 @@
 import requests
 import urllib
 import os
+import json
 from dotenv import load_dotenv
 
-load_dotenv()
 
-secret_key = os.getenv("SECRET_KEY")
-base_url = os.getenv('BASE_URL')
+def korea_city_code1(secret_key, base_url):
+    suffix = "/areaCode1"
+    url = base_url + suffix
 
-params = {
-    "serviceKey": secret_key,
-    "numOfRows": 10,
-    "pageNo": 1,
-    "MobileOS": "ETC",
-    "MobileApp": "AppTest",
-    "_type": "json",
-    "listYN": "Y",
-    "arrange": "A",
-    "contentTypeId": "32",
-    "areaCode": "4",
-    "sigunguCode": "4",
-    "cat1": "B02",
-    "cat2": "B0201",
-    "cat3": "B02010100",
-    "modifiedtime": "20220721"
-}
+    params = {
+        "serviceKey": secret_key,
+        "numOfRows": 10,
+        "pageNo": 1,
+        "MobileOS": "ETC",
+        "MobileApp": "TripTune",
+        "areaCode": "",
+        "_type": "json",
+    }
 
-params = urllib.parse.urlencode(params, safe='#\':()+=%,')
+    content = get_json_data(url, params)
+    total_count = content["response"]["body"]['totalCount']
+    pageNo = 0
 
-response = requests.get(base_url, params=params)
-content = response.text
+    while pageNo * 10 < total_count:
+        pageNo += 1
+        params["pageNo"] = pageNo
 
-print(content)
+        content = get_json_data(url, params)
+        print(content)
+
+    return total_count
+
+
+def get_json_data(url, params):
+    encoding_params = urllib.parse.urlencode(params, safe='#\':()+=%,')
+
+    response = requests.get(url, params=encoding_params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"요청 실패: 상태코드 {response.status_code}")
+
+
+
+def main():
+    load_dotenv()
+
+    secret_key = os.getenv("SECRET_KEY")
+    base_url = os.getenv("BASE_URL")
+
+    if not secret_key or not base_url:
+        print("환경 변수 불러오기 실패")
+        return
+
+    korea_city_code1(secret_key, base_url)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
