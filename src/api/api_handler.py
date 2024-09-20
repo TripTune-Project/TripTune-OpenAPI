9cfba8624413a60cbaba8c13fa865bb9f7a589db
Heesoo <soyamilk0705@naver.com>
2024-09-20 17:05:06 +0900

    [feat] api 결과 중 1 page 데이터만 리스트로 만드는 함수 추가 #3

--- src/api/api_handler.py
+++ src/api/api_handler.py
@@ -93,3 +93,31 @@ def fetch_items(url, params, total_count):
         items.extend(content['response']['body']['items']['item'])
         
     return items
+
+
+
+def fetch_one_page_items(url, params):
+    '''
+    get_json_data 함수를 통해 api 요청해 요청 결과에 item 만 추출해서 리스트에 저장한다.
+    pageNo = 1 로 설정해 최대 10개의 데이터만 요청하도록 한다.
+
+    *api 요청 결과는 {'response':{'header':{...}, 'body':{'items':'item':[]}}} 으로 구성되어 있다.
+
+    [Parameter]
+    url: open api 요청 url
+    params: open api 요청 파라미터
+    total_count: 요청 결과인 아이템 총 갯수
+
+    [Return]
+    items : 요청 결과 중 item 값만 담은 리스트
+    '''
+    items = []
+
+    params['pageNo'] = 1
+
+    content = get_json_data(url, params)
+    print(content)
+    
+    items.extend(content['response']['body']['items']['item'])
+        
+    return items
