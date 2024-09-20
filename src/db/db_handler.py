15565eae3e58dc177b086f9b7448caf8c2cc0520
Heesoo <soyamilk0705@naver.com>
2024-09-20 17:05:45 +0900

    [feat] 나라, 도시, 시군구를 이용해서 지역 데이터를 조회하는 쿼리 추가 #3

--- src/db/db_handler.py
+++ src/db/db_handler.py
@@ -21,6 +21,24 @@ class DatabaseHandler:
         self.cursor.execute(query, params)
         return self.cursor.fetchone()
 
+    def execute_select_area(self, country, city, district):
+        select_city_and_district = '''SELECT
+                                        ct.country_id, ct.country_name,
+                                        c.city_id, c.city_name, c.api_area_code,
+                                        d.district_id, d.district_name, d.api_sigungu_code
+                                    FROM district d
+                                    INNER JOIN city c
+                                    ON d.city_id = c.city_id
+                                    INNER JOIN country ct
+                                    ON c.country_id = ct.country_id
+                                    WHERE ct.country_name = %s
+                                    AND c.city_name = %s
+                                    AND d.district_name = %s
+                                    '''
+                                
+        self.cursor.execute(select_city_and_district, (country, city, district))
+        return self.cursor.fetchone()
+
     def execute_insert(self, query, params):
         self.cursor.execute(query, params)
         self.conn.commit()
